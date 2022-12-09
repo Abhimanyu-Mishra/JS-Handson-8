ecda105ec501e158053a6e9f66d708c82fd5f24f
= <=>
2022-12-09 12:47:33 +0530

    array of object

---
+++ main.js
@@ -0,0 +1,57 @@
+let studentRecords = [ {name: 'John', id: 123, marks : 98 },
+          {name: 'Baba', id: 101, marks : 23 },
+          {name: 'yaga', id: 200, marks : 45 },
+          {name: 'Wick', id: 115, marks : 75 } ] 
+
+    // for( let i = 0; i <= studentRecords.length; i++){
+    //     arr = []
+    //  var a = studentRecords[i].marks;
+    //       arr.push(a)
+
+    //       if(arr >50){
+    //         console.log(arr)
+    //       }
+          
+    // }
+    //  let stdarr = studentRecords.map((a) => a.name.toUpperCase())
+    // console.log(stdarr)
+    
+//    let stdarr = studentRecords.filter((a) => a.marks>50)
+//    console.log(stdarr);
+
+
+
+// let stdid = studentRecords.filter((a) =>a.id>120)
+
+// let stdgre = stdid.filter((a) =>a.marks>50) 
+// console.log(stdgre);
+
+//  let summarks = studentRecords.reduce((a,b)=> a+=b.marks ,0)
+//  console.log(summarks);
+
+// let maxmarks = studentRecords.filter((a) => a.marks>50)
+// let stdname = maxmarks.map((a) => a.name)
+
+// let maxid = studentRecords.filter((a) => a.id>120)
+
+// let stdsum = maxid.reduce((a,b) => a+=b.marks, 0)
+// console.log(stdsum);
+
+
+// let maxmarks = studentRecords.filter((a) => a.marks>50)
+// let summ = maxmarks.reduce((a,b) => a += b.marks, 0)
+
+// let below = studentRecords.filter((a)=>a.marks<50)
+// let add = below.map((a)=>a.marks+15);
+// let great = add.filter((a) => a>50)
+// let summgr = great.reduce((a,b) =>a += b ,0)
+
+// total = summgr + summ
+
+
+arr=[]
+const arrayObj = new Array()
+new Array() = 'Name' ,'class','rollno'
+
+
+ console.log(arr);
